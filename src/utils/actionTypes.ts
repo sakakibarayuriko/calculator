f1cb8ff72361f293279f43de024ef8cdec06444a
sakakibarayuriko <sakakibarayuriko@team-lab.com>
2021-06-26 17:31:10 +0900

    actionType追加

---
+++ src/utils/actionTypes.ts
@@ -0,0 +1,10 @@
+/** 数字 */
+export const INPUT_NUMBER = "INPUT_NUMBER";
+/** 小数点 */
+export const INPUT_POINT = "INPUT_POINT";
+/** 四則演算子 */
+export const OPERATION = "OPERATION";
+/** イコール */
+export const EQUAL = "EQUAL";
+/** リセット */
+export const RESET = "RESET";
