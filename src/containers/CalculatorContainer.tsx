aaed6eccc65826bdaf59fa09e89ba9975fb4b938
sakakibarayuriko <sakakibarayuriko@team-lab.com>
2021-06-27 01:42:39 +0900

    css追加

--- src/containers/CalculatorContainer.tsx
+++ src/containers/CalculatorContainer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { GetAllActions } from "../actions";
 import Button from "../components/Button";
+import Formula from "../components/Formula";
 import Result from "../components/Result";
 import { AllState } from "../store";
 import { OPT } from "../types";
@@ -26,34 +27,45 @@ export const CalculatorContainer = () => {
     };
     return (
         <div>
-            <Result result={calculator.formula + calculator.inputValue} />
-            <Result result={calculator.resultValue} />
-            <div>
-                <Button character={"c"} onClick={onResetClick} />
-                <Button character={"÷"} onClick={() => onOperationClick("÷")} />
+            <div className="top"></div>
+            <div className="result">
+                <Formula formula={calculator.formula + calculator.inputValue} />
+                <Result result={calculator.resultValue} />
             </div>
-            <div>
-                <Button character={7} onClick={() => onNumClick(7)} />
-                <Button character={8} onClick={() => onNumClick(8)} />
-                <Button character={9} onClick={() => onNumClick(9)} />
-                <Button character={"×"} onClick={() => onOperationClick("×")} />
-            </div>
-            <div>
-                <Button character={4} onClick={() => onNumClick(4)} />
-                <Button character={5} onClick={() => onNumClick(5)} />
-                <Button character={6} onClick={() => onNumClick(6)} />
-                <Button character={"-"} onClick={() => onOperationClick("-")} />
-            </div>
-            <div>
-                <Button character={1} onClick={() => onNumClick(1)} />
-                <Button character={2} onClick={() => onNumClick(2)} />
-                <Button character={3} onClick={() => onNumClick(3)} />
-                <Button character={"+"} onClick={() => onOperationClick("+")} />
-            </div>
-            <div>
-                <Button character={0} onClick={() => onNumClick(0)} />
-                <Button character={"."} onClick={() => onPointClick(true)} />
-                <Button character={"="} onClick={onEqualClick} />
+            <div className="wrapper">
+                <div className="number">
+                    <div className="clear">
+                        <Button character={"C"} onClick={onResetClick} />
+                    </div>
+                    <div className="numUpper">
+                        <Button character={7} onClick={() => onNumClick(7)} />
+                        <Button character={8} onClick={() => onNumClick(8)} />
+                        <Button character={9} onClick={() => onNumClick(9)} />
+                    </div>
+                    <div className="numMiddleUpper">
+                        <Button character={4} onClick={() => onNumClick(4)} />
+                        <Button character={5} onClick={() => onNumClick(5)} />
+                        <Button character={6} onClick={() => onNumClick(6)} />
+                    </div>
+                    <div className="numMiddleLower">
+                        <Button character={1} onClick={() => onNumClick(1)} />
+                        <Button character={2} onClick={() => onNumClick(2)} />
+                        <Button character={3} onClick={() => onNumClick(3)} />
+                    </div>
+                    <div className="numLower">
+                        <div className="zero">
+                            <Button character={0} onClick={() => onNumClick(0)} />
+                        </div>
+                        <Button character={"."} onClick={() => onPointClick(true)} />
+                    </div>
+                </div>
+                <div className="operator">
+                    <Button character={"÷"} onClick={() => onOperationClick("÷")} />
+                    <Button character={"×"} onClick={() => onOperationClick("×")} />
+                    <Button character={"-"} onClick={() => onOperationClick("-")} />
+                    <Button character={"+"} onClick={() => onOperationClick("+")} />
+                    <Button character={"="} onClick={onEqualClick} />
+                </div>
             </div>
         </div>
     );
